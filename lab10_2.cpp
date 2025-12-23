cd5baa4f302762d5308a56ea8c4b9881a75df268
Methas <methas_n@cmu.ac.th>
2025-12-23 23:43:12 +0700

    "lab10_2.cpp completed"

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,18 +1,26 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
+int main()
+{
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest<<("-------------------- BOOM ---------------------\n");
+	
+	string text;
+	while(getline(source,text)){
+		dest<<text<<endl;
+	}
+	dest<<("-------------------- HA!! ---------------------");
+		// กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ
+		// โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
-    source.close();
-    dest.close();
+	source.close();
+	dest.close();
 	return 0;
 }
